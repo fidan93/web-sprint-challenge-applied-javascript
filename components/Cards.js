aeeb31ba4a9c34e721475cb0bbdc5c0ac95b79d3
Fidan Novruzova <yf@Fidans-MacBook-Pro.local>
2020-10-30 09:30:45 -0700

    completed mvp

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,51 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+import axios from "axios";
+
+function createMarkup(obj){
+const card = document.createElement("div");
+const headline = document.createElement("div");
+const author = document.createElement("div");
+const imgCont = document.createElement("div");
+const image = document.createElement("img");
+const by = document.createElement("span");
+
+card.classList.add("card");
+headline.classList.add("headline");
+author.classList.add("author");
+imgCont.classList.add("img-container")
+
+headline.textContent = obj.headline;
+image.src = obj.authorPhoto;
+by.textContent = `By ${obj.authorName}`;
+
+card.appendChild(headline);
+card.appendChild(author);
+author.appendChild(imgCont);
+author.appendChild(by);
+imgCont.appendChild(image);
+return card;
+
+}
+
+axios
+.get("https://lambda-times-api.herokuapp.com/articles")
+.then((res) => {
+    const hline = res.data.articles;
+    const cards = document.querySelector(".cards-container");
+for(let key in hline){
+    const auth = hline[key];
+    console.log(auth);
+    auth.forEach((el) => {
+console.log(el);
+cards.appendChild(createMarkup(el));
+    })
+
+}
+    
+})
+.catch((err)=> {
+    console.log(err);
+})
